5b8d16db62ea1b8a3afe7e8eaa0bba8d9724f42b
RaduTek <radutek13@gmail.com>
2024-04-05 23:01:50 +0300

    Index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
-export default function Home() {
-  return <></>;
+import { Typography } from "@mui/material";
+
+export default function IndexPage() {
+  return <Typography variant="h4">Welcome to API Wishman!</Typography>;
 }
