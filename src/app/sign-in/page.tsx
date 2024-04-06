11cadfce498e8d96662bccc76c2d22f128837846
AndyStan112 <stanica.andrei28@gmail.com>
2024-04-06 06:30:43 +0300

    radu citeste pls

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,53 +2,55 @@
 import { useState } from 'react';
 import {useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth'
 import {auth} from '@/app/firebase/config'
+import { useRouter } from 'next/navigation';
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('')
   const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const [error, setError] = useState('')
+  const router = useRouter()
 
   const handleSignIn = async () => {
     try {
-        const res = await signInWithEmailAndPassword(email, password)
-        console.log({res})
-        sessionStorage.setItem('user', true)
-        setEmail('');
-        setPassword('')
-
-    } catch(e){
-        console.error(e)
-        setError(e.message)
-    }
-  };
+      const res = await signInWithEmailAndPassword(email, password);
+      console.log(res);
+      //daca res e undefined a dat fail , la fel si la sign up, ma culc, success. Nu reusesc sa scot erorile, catchul e degeaba
+      sessionStorage.setItem('user', true);
+      setEmail('');
+      setPassword('');
+      router.push('/');
+  } catch (error) {
+      console.error("Sign-in failed:", error);
+      setError(error.message); 
+  }}
 
   return (
-    <div>
-      <div >
-        <h1 >Sign In</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+        <h1 className="text-white text-2xl mb-5">Sign In</h1>
         <input 
           type="email" 
           placeholder="Email" 
           value={email} 
           onChange={(e) => setEmail(e.target.value)} 
-          className=""
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
         <input 
           type="password" 
           placeholder="Password" 
           value={password} 
           onChange={(e) => setPassword(e.target.value)} 
-         
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
         />
         <button 
           onClick={handleSignIn}
-         
+          className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
         >
           Sign In
         </button>
+        <p>{error}</p>
       </div>
-      <p>{error}</p>
     </div>
   );
 };
