ad81e81b24f586f9a18f3d0bc602794705ea67a8
Dragan Iulian <iulianalexandrudragan@gmail.com>
2024-04-06 16:40:22 +0300

    feat: add /bug/solve/[id] endpoint

---
+++ src/pages/api/bug/solve/[id].ts
@@ -0,0 +1,41 @@
+import prisma from "@/prismadb";
+import { NextApiRequest, NextApiResponse } from "next";
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const id = req.query.id as string;
+    const { userId } = req.body;
+
+    const bug_report = await prisma.bugReport.findFirst({ where: { id: id } });
+    const app = await prisma.app.findFirst({
+      where: { id: bug_report?.appId as string },
+    });
+    if (app?.userId != userId) {
+      res
+        .status(403)
+        .send({
+          error: "User doesn't have permission to mark this bug as solved",
+        });
+    }
+
+    await prisma.bugReport.update({
+      where: { id: id },
+      data: {
+        solveDate: new Date(Date.now()),
+      },
+    });
+
+    await prisma.app.update({
+      where: {
+        id: app?.id,
+      },
+      data: { isBugged: false },
+    });
+
+    res.status(200).json({ status: "ok" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal server error" });
+  }
+};
+
+export default handler;
