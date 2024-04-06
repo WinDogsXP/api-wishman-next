19047c77e86ff4c1a8d789d6412315f40984d0b6
RaduTek <radutek13@gmail.com>
2024-04-06 08:32:11 +0300

    Fetch app list from backend

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -1,28 +1,60 @@
+"use client";
 import AppListItem from "@/components/AppListItem";
-import { Box, List, Paper, Typography } from "@mui/material";
+import { AppInfo } from "@/types";
+import { Box, Divider, List, Paper, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function ApplicationsPage() {
+  const [loading, setLoading] = useState(true);
+  const [appList, setAppList] = useState<Array<AppInfo>>([]);
+
+  const getApps = () => {
+    fetch("/api/apps/get")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        if (res.apps.length == 0) {
+          console.log("No apps!");
+        }
+        setAppList(res.apps);
+        setLoading(false);
+      })
+      .catch((reason) => {
+        console.log(reason);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    if (loading) getApps();
+  }, [loading]);
+
   return (
     <Box>
       <Typography variant="h5" sx={{ mb: 1.2 }}>
         Applications
       </Typography>
       <Paper>
-        <List sx={{ p: 0 }}>
-          <AppListItem
-            appInfo={{ id: "abc", name: "Application Name", status: "stable" }}
-          />
-          <AppListItem
-            appInfo={{
-              id: "def",
-              name: "Application Name",
-              status: "unstable",
-            }}
-          />
-          <AppListItem
-            appInfo={{ id: "ghi", name: "Application Name", status: "down" }}
-          />
-        </List>
+        {loading ? (
+          <Box sx={{ p: 7, textAlign: "center" }}>
+            <Typography variant="h5">Loading...</Typography>
+          </Box>
+        ) : appList.length > 0 ? (
+          <List sx={{ p: 0 }}>
+            {appList.map((appInfo, index) => (
+              <>
+                <AppListItem key={index} appInfo={appInfo} />
+                {index < appList.length - 1 && (
+                  <Divider variant="inset" component="li" />
+                )}
+              </>
+            ))}
+          </List>
+        ) : (
+          <Box sx={{ p: 7, textAlign: "center" }}>
+            <Typography variant="h5">No applications!</Typography>
+          </Box>
+        )}
       </Paper>
     </Box>
   );
