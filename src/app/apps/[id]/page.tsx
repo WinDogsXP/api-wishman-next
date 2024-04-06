aefa87d58edd133cca89cc979a6d86bb4fb0b019
Dragan Iulian <iulianalexandrudragan@gmail.com>
2024-04-06 16:20:59 +0300

    feat: add /app/[id] page

--- src/app/apps/[id]/page.tsx
+++ src/app/apps/[id]/page.tsx
@@ -1,11 +1,33 @@
+"use client";
+
 import PageHeader from "@/components/PageHeader";
 import { Box, Paper, Typography } from "@mui/material";
+import { enqueueSnackbar } from "notistack";
+import { useEffect } from "react";
+
+const apiUrl = "/api/apps/get/";
 
 export default function ApplicationPage({
   params,
 }: {
   params: { id: string };
 }) {
+  function loadApp() {
+    fetch(apiUrl + params.id)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+      })
+      .catch((e) => {
+        console.log(e);
+        enqueueSnackbar<"error">("Error loading application!");
+      });
+  }
+
+  useEffect(() => {
+    loadApp();
+  }, []);
+
   return (
     <Box>
       <PageHeader title="Application" backButton={true} />
