6b4c1a56fd7d1cd8445e92910b5cb6a6d2556787
RaduTek <radutek13@gmail.com>
2024-04-06 12:46:27 +0300

    Update dashboard

--- src/app/dev/page.tsx
+++ src/app/dev/page.tsx
@@ -1,30 +1,39 @@
 "use client";
 
-import { Button, Typography } from "@mui/material";
+import { Button, LinearProgress, Paper, Typography } from "@mui/material";
 import { auth } from "../firebase/config";
 import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
+import PageHeader from "@/components/PageHeader";
 
 export default function DashboardPage() {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const [signOut, loadingSO, errorSO] = useSignOut(auth);
 
-  return user ? (
+  return (
     <>
-      <Typography variant="h5">Developer Dashboard</Typography>
-      <Button
-        onClick={async () => {
-          const success = await signOut();
-          if (success) {
-            alert("You are sign out");
-          }
-        }}
-      >
-        Log out
-      </Button>
-    </>
-  ) : (
-    <>
-      <Typography>You are not logged in.</Typography>
+      <PageHeader title="Developer Dashboard" />
+      {loading ? (
+        <LinearProgress />
+      ) : user ? (
+        <>
+          <Paper sx={{ p: 1 }}>
+            <Button
+              onClick={async () => {
+                const success = await signOut();
+                if (success) {
+                  alert("You are sign out");
+                }
+              }}
+            >
+              Log out
+            </Button>
+          </Paper>
+        </>
+      ) : (
+        <Paper sx={{ p: 1 }}>
+          <Typography>You are not logged in.</Typography>
+        </Paper>
+      )}
     </>
   );
 }
