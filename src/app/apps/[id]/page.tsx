a1153e2c87de0960be3a22741a73434358d1c8f0
RaduTek <radutek13@gmail.com>
2024-04-06 17:06:44 +0300

    Add endpoint list to app page

--- src/app/apps/[id]/page.tsx
+++ src/app/apps/[id]/page.tsx
@@ -1,9 +1,24 @@
 "use client";
-
+import { auth } from "@/app/firebase/config";
+import EndpointListItem from "@/components/EndpointListItem/EndpointListItem";
 import PageHeader from "@/components/PageHeader";
-import { Box, Paper, Typography } from "@mui/material";
+import { AppInfo, Endpoint } from "@/types";
+import handleRouterPush from "@/util/handleRouterPush";
+import { Add } from "@mui/icons-material";
+import {
+  Paper,
+  Stack,
+  Typography,
+  Divider,
+  IconButton,
+  LinearProgress,
+  List,
+  Tooltip,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
 import { enqueueSnackbar } from "notistack";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const apiUrl = "/api/apps/get/";
 
@@ -12,15 +27,33 @@ export default function ApplicationPage({
 }: {
   params: { id: string };
 }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const [appInfo, setAppInfo] = useState<AppInfo>();
+  const [loading, setLoading] = useState(true);
+
   function loadApp() {
     fetch(apiUrl + params.id)
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
+        const _appInfo = json.app as AppInfo;
+
+        // Add appId to each endpoint object
+        const _endpoints = [] as Endpoint[];
+        _appInfo.endpoint?.forEach((endpoint, index) => {
+          console.log(endpoint);
+          _endpoints[index] = endpoint;
+          _endpoints[index].appId = _appInfo.id;
+        });
+        _appInfo.endpoint = _endpoints;
+
+        setAppInfo(_appInfo);
+        setLoading(false);
       })
       .catch((e) => {
         console.log(e);
-        enqueueSnackbar<"error">("Error loading application!");
+        enqueueSnackbar<"error">("Error loading application details!");
+        setLoading(false);
       });
   }
 
@@ -29,11 +62,47 @@ export default function ApplicationPage({
   }, []);
 
   return (
-    <Box>
+    <>
       <PageHeader title="Application" backButton={true} />
-      <Paper sx={{ p: 1 }}>
-        <Typography>ID: {params.id}</Typography>
-      </Paper>
-    </Box>
+      <Stack gap={1}>
+        <Paper sx={{ p: 1 }}>
+          <Typography>ID: {params.id}</Typography>
+        </Paper>
+
+        <Stack>
+          <PageHeader small title="Endpoints">
+            {user && appInfo && user?.uid == appInfo?.userId && (
+              <Tooltip title="Create new endpoint">
+                <IconButton
+                  component="a"
+                  href="/apps/new"
+                  onClick={handleRouterPush(router)}
+                >
+                  <Add />
+                </IconButton>
+              </Tooltip>
+            )}
+          </PageHeader>
+          <LinearProgress
+            style={{ visibility: loading ? "visible" : "hidden" }}
+          />
+          <Paper>
+            {appInfo?.endpoint && (
+              <List sx={{ p: "0 !important" }}>
+                {appInfo.endpoint.map((endpointInfo, index) => (
+                  <div key={index}>
+                    <EndpointListItem endpointInfo={endpointInfo} />
+                    {appInfo.endpoint &&
+                      index < appInfo.endpoint.length - 1 && (
+                        <Divider variant="inset" />
+                      )}
+                  </div>
+                ))}
+              </List>
+            )}
+          </Paper>
+        </Stack>
+      </Stack>
+    </>
   );
 }
