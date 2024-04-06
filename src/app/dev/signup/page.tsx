86a6d06c6ac2a770592289f540801eac7b70c570
RaduTek <radutek13@gmail.com>
2024-04-06 10:28:38 +0300

    Update signup page

--- src/app/dev/signup/page.tsx
+++ src/app/dev/signup/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FormEvent, useState } from "react";
+import { FormEvent } from "react";
 import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 import {
@@ -9,29 +9,30 @@ import {
   Paper,
   TextField,
   Typography,
+  useTheme,
 } from "@mui/material";
-import { CheckCircle } from "@mui/icons-material";
+import { CheckCircle, Error } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
 
 const SignUp = () => {
   const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(auth);
+  const router = useRouter();
+  const theme = useTheme();
 
   const handleSignUp = async (event: FormEvent) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget as HTMLFormElement);
-    console.log(formData);
-    const res = await createUserWithEmailAndPassword(
+    await createUserWithEmailAndPassword(
       formData.get("email") as string,
       formData.get("password") as string
     );
-    console.log(res);
-    sessionStorage.setItem("user", "true");
+    console.log(user);
     return false;
   };
 
   return (
     <Paper
-      component="form"
       sx={{
         p: 1.5,
         maxWidth: "400px",
@@ -41,7 +42,6 @@ const SignUp = () => {
         gap: 2,
         userSelect: "none",
       }}
-      onSubmit={handleSignUp}
     >
       <Box>
         <Typography variant="h5">Sign Up</Typography>
@@ -60,62 +60,78 @@ const SignUp = () => {
           >
             <CheckCircle sx={{ fontSize: "48px" }} />
             <Typography>Account created successfully!</Typography>
-            <Button variant="contained" type="submit">
-              Go to My Account
+            <Button
+              component="a"
+              href="/dev"
+              variant="contained"
+              type="submit"
+              onClick={(e) => {
+                e.preventDefault();
+                router.push("/dev");
+              }}
+            >
+              Go to Dashboard
             </Button>
           </Box>
         </>
       ) : (
-        <>
+        <Box
+          component="form"
+          onSubmit={handleSignUp}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 1.6,
+          }}
+        >
           <Typography>Create a new account</Typography>
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            <TextField
-              variant="outlined"
-              name="email"
-              type="email"
-              label="Email Address"
-              required
-            />
-            <TextField
-              variant="outlined"
-              name="password"
-              type="password"
-              label="Password"
-              required
-            />
-          </Box>
+          {error && (
+            <Paper
+              sx={{
+                backgroundColor: theme.palette.error.main,
+                color: theme.palette.error.contrastText,
+                p: 1,
+                display: "flex",
+                gap: 1,
+              }}
+            >
+              <Error />
+              <Typography>{error?.message}</Typography>
+            </Paper>
+          )}
+          <TextField
+            variant="outlined"
+            name="email"
+            type="email"
+            label="Email Address"
+            disabled={loading}
+            sx={{ mt: 1 }}
+            required
+          />
+          <TextField
+            variant="outlined"
+            name="password"
+            type="password"
+            label="Password"
+            disabled={loading}
+            required
+          />
           <Box sx={{ display: "flex", gap: 1.5, mx: "auto" }}>
-            <Button variant="contained" color="success" type="submit">
+            <Button
+              variant="contained"
+              color="success"
+              type="submit"
+              disabled={loading}
+            >
               Sign Up
             </Button>
-            <Button variant="outlined" type="reset">
+            <Button variant="outlined" type="reset" disabled={loading}>
               Cancel
             </Button>
           </Box>
-        </>
+        </Box>
       )}
     </Paper>
-    // <div>
-    //   <div>
-    //     <h1>Sign Up</h1>
-    //     <input
-    //       type="email"
-    //       placeholder="Email"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //       className=""
-    //     />
-    //     <input
-    //       type="password"
-    //       placeholder="Password"
-    //       value={password}
-    //       onChange={(e) => setPassword(e.target.value)}
-    //     />
-    //     <button onClick={handleSignUp}>Sign Up</button>
-    //   </div>
-    //   <p>{errorState}</p>
-    //   <p>{error?.message}</p>
-    // </div>
   );
 };
 
