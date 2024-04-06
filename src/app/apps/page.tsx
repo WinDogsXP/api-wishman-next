bc29e7ff780de56dbdfdcbd7b6cbde596e5c7fd2
RaduTek <radutek13@gmail.com>
2024-04-06 10:47:44 +0300

    Add loading status and refresh button

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 import AppListItem from "@/components/AppListItem";
 import { AppInfo } from "@/types";
-import { Box, Divider, List, Paper, Typography } from "@mui/material";
+import { Refresh, Visibility } from "@mui/icons-material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  LinearProgress,
+  List,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 
 export default function ApplicationsPage() {
   const [loading, setLoading] = useState(true);
   const [appList, setAppList] = useState<Array<AppInfo>>([]);
 
-  const getApps = () => {
+  const loadApps = () => {
+    setLoading(true);
     fetch("/api/apps/get")
       .then((res) => res.json())
       .then((res) => {
-        console.log(res);
         if (res.apps.length == 0) {
           console.log("No apps!");
         }
@@ -26,20 +35,27 @@ export default function ApplicationsPage() {
   };
 
   useEffect(() => {
-    if (loading) getApps();
+    if (loading) loadApps();
   }, [loading]);
 
   return (
     <Box>
-      <Typography variant="h5" sx={{ mb: 1.2 }}>
-        Applications
-      </Typography>
+      <Box sx={{ display: "flex", gap: 1.2, mb: 1.2 }}>
+        <Typography variant="h5" sx={{ flexGrow: 1, pl: 1, py: 0.5 }}>
+          Applications
+        </Typography>
+        <IconButton
+          disabled={loading}
+          onClick={() => {
+            loadApps();
+          }}
+        >
+          <Refresh />
+        </IconButton>
+      </Box>
+      <LinearProgress style={{ visibility: loading ? "visible" : "hidden" }} />
       <Paper>
-        {loading ? (
-          <Box sx={{ p: 7, textAlign: "center" }}>
-            <Typography variant="h5">Loading...</Typography>
-          </Box>
-        ) : appList.length > 0 ? (
+        {appList.length > 0 ? (
           <List sx={{ p: 0 }}>
             {appList.map((appInfo, index) => (
               <>
@@ -51,9 +67,11 @@ export default function ApplicationsPage() {
             ))}
           </List>
         ) : (
-          <Box sx={{ p: 7, textAlign: "center" }}>
-            <Typography variant="h5">No applications!</Typography>
-          </Box>
+          !loading && (
+            <Box sx={{ p: 7, textAlign: "center" }}>
+              <Typography variant="h5">No applications!</Typography>
+            </Box>
+          )
         )}
       </Paper>
     </Box>
