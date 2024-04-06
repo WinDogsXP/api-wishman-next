1071eeb1378757185b6db1b97abe697daac74407
RaduTek <radutek13@gmail.com>
2024-04-06 12:46:15 +0300

    Use page header

--- src/app/apps/[id]/page.tsx
+++ src/app/apps/[id]/page.tsx
@@ -1,3 +1,4 @@
+import PageHeader from "@/components/PageHeader";
 import { Box, Paper, Typography } from "@mui/material";
 
 export default function ApplicationPage({
@@ -7,10 +8,8 @@ export default function ApplicationPage({
 }) {
   return (
     <Box>
-      <Typography variant="h5" sx={{ mb: 1.2 }}>
-        Application
-      </Typography>
-      <Paper>
+      <PageHeader title="Application" backButton={true} />
+      <Paper sx={{ p: 1 }}>
         <Typography>ID: {params.id}</Typography>
       </Paper>
     </Box>
