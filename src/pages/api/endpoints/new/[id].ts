9986fe16fbb8417e7bce8afb38dac2937a6ea0f5
AndyStan112 <stanica.andrei28@gmail.com>
2024-04-06 14:40:41 +0300

    add endpoints to app

---
+++ src/pages/api/endpoints/new/[id].ts
@@ -0,0 +1,43 @@
+import prisma from "@/prismadb";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const id = req.query.id as string;
+    const { userId, interval, url, headers, method, body, name } = req.body;
+
+    const userEndpoints = await prisma.app.findFirst({
+      where: {
+        userId: userId,
+        endpoint: {
+          some: {
+            id,
+          },
+        },
+      },
+    });
+
+    if (!userEndpoints) {
+      return res.status(403).json({ error: "User does not own the app " });
+    }
+
+    const updatedEndpoint = await prisma.endpoint.create({
+      data: {
+        appId: id,
+        interval,
+        url,
+        headers,
+        method,
+        body,
+        name,
+      },
+    });
+
+    res.status(200).json({ updatedEndpoint });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal server error" });
+  }
+};
+
+export default handler;
